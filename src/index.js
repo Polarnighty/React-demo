7a69462eb21acaa0e5f34eb16d0edec749361c72
polarnight <1041465280@qq.com>
2019-09-30 23:51:34 +0800

    初步完成五子棋

--- src/index.js
+++ src/index.js
@@ -1,12 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDom from 'react-dom'
+import './style.css'
+const Cell = function (props) {
+    return (
+        <div className="cell" onClick={props.onClick}>
+            {props.text}
+        </div>
+    )
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Chessboard = () => {
+    const [cells, setCells] = React.useState([
+        [null, null, null],
+        [null, null, null],
+        [null, null, null],
+    ])
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    const [n, setN] = React.useState(0)
+    const [finished, setFinished] = React.useState(false)
+    const tell = (cells) => {
+        for (let i = 0; i < 3; i++) {
+            if (cells[i][0] === cells[0][1] && cells[i][1] === cells[i][2]
+                && cells[i][0] !== null
+            ) {
+                console.log(cells[i][0] + '赢了')
+                setFinished(true)
+                break
+            }
+        }
+        for (let i = 0; i < 3; i++) {
+            if (cells[0][i] === cells[0][i] && cells[0][i] === cells[0][i]
+                && cells[0][i] !== null
+            ) {
+                console.log(cells[0][i] + '赢了')
+                setFinished(true)
+                break
+            }
+        }
+    }
+    const onClickCell = (row, col) => {
+        setN(n + 1)
+        const copy = JSON.parse(JSON.stringify(cells))
+        setCells(copy)
+        copy[row][col] = n % 2 === 0 ? 'x' : 'o'
+        setCells(copy)
+        tell(copy)
+    }
+    return (
+        <div>
+            {cells.map((items, row) => <div className="row">
+                {items.map(
+                    (item, col) => <div className="col">
+                        <Cell text={item} onClick={() => onClickCell(row, col)} />
+                    </div>)
+                }</div>)}
+        </div>
+    )
+}
+ReactDom.render(<div>
+    <Chessboard />
+</div>, document.getElementById('root'))
